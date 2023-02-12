e0d9932fa47bcf10144d1c42ffbcb2f3d7e4d992
Netkas <zixing.narrakas1996@gmail.com>
2023-02-12 18:38:13 -0500

    Added \TgBotLib\Objects > MessageAutoDeleteTimerChanged

---
+++ src/TgBotLib/Objects/MessageAutoDeleteTimerChanged.php
@@ -0,0 +1,49 @@
+<?php
+
+    /** @noinspection PhpMissingFieldTypeInspection */
+
+    namespace TgBotLib\Objects;
+
+    use TgBotLib\Interfaces\ObjectTypeInterface;
+
+    class MessageAutoDeleteTimerChanged implements ObjectTypeInterface
+    {
+        /**
+         * @var int
+         */
+        private $message_auto_delete_time;
+
+        /**
+         * @return int
+         */
+        public function getMessageAutoDeleteTime(): int
+        {
+            return $this->message_auto_delete_time;
+        }
+
+        /**
+         * Returns an array representation of the object.
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return [
+                'message_auto_delete_time' => $this->message_auto_delete_time,
+            ];
+        }
+
+        /**
+         * Constructs the object from an array representation.
+         *
+         * @param array $data
+         * @return ObjectTypeInterface
+         */
+        public static function fromArray(array $data): ObjectTypeInterface
+        {
+            $object = new self();
+            $object->message_auto_delete_time = $data['message_auto_delete_time'];
+
+            return $object;
+        }
+    }
