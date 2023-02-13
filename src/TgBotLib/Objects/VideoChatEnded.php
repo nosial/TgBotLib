223baa8fa16259c222fdbd01b179d5efeab77253
Netkas <zixing.narrakas1996@gmail.com>
2023-02-12 21:52:45 -0500

    Added \TgBotLib\Objects > VideoChatEnded.php

---
+++ src/TgBotLib/Objects/VideoChatEnded.php
@@ -0,0 +1,50 @@
+<?php
+
+    namespace TgBotLib\Objects;
+
+    use TgBotLib\Interfaces\ObjectTypeInterface;
+
+    class VideoChatEnded implements ObjectTypeInterface
+    {
+        /**
+         * @var int
+         */
+        private $duration;
+
+        /**
+         * Video chat duration in seconds
+         *
+         * @return int
+         */
+        public function getDuration(): int
+        {
+            return $this->duration;
+        }
+
+        /**
+         * Returns an array representation of the object
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return [
+                'duration' => $this->duration,
+            ];
+        }
+
+        /**
+         * Constructs object from an array representation
+         *
+         * @param array $data
+         * @return ObjectTypeInterface
+         */
+        public static function fromArray(array $data): ObjectTypeInterface
+        {
+            $object = new self();
+            $object->duration = $data['duration'];
+            return $object;
+        }
+
+
+    }
