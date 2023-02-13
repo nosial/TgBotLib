8926b42dcac71f2f3488e7433b98db2d734d9b47
Netkas <zixing.narrakas1996@gmail.com>
2023-02-12 21:54:26 -0500

    Added \TgBotLib\Objects > VideoChatParticipantsInvited

---
+++ src/TgBotLib/Objects/VideoChatParticipantsInvited.php
@@ -0,0 +1,45 @@
+<?php
+
+    /** @noinspection PhpMissingFieldTypeInspection */
+
+    namespace TgBotLib\Objects;
+
+    use TgBotLib\Interfaces\ObjectTypeInterface;
+
+    class VideoChatParticipantsInvited implements ObjectTypeInterface
+    {
+
+        /**
+         * @var User[]
+         */
+        private $users;
+
+        /**
+         * Returns an array representation of the object
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return [
+                'users' => array_map(function (User $user) {
+                    return $user->toArray();
+                }, $this->users),
+            ];
+        }
+
+        /**
+         * Constructs object from an array representation
+         *
+         * @param array $data
+         * @return ObjectTypeInterface
+         */
+        public static function fromArray(array $data): ObjectTypeInterface
+        {
+            $object = new self();
+            $object->users = array_map(function (array $user) {
+                return User::fromArray($user);
+            }, $data['users']);
+            return $object;
+        }
+    }
