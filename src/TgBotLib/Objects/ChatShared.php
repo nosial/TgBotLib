6ee9f489400a9023a6a9122f34341d1dc3888113
Netkas <zixing.narrakas1996@gmail.com>
2023-02-12 21:46:28 -0500

    Added missing docs to \TgBotLib\Objects > ChatShared

--- src/TgBotLib/Objects/ChatShared.php
+++ src/TgBotLib/Objects/ChatShared.php
@@ -19,6 +19,8 @@
         private $chat_id;
 
         /**
+         * Identifier of the request
+         *
          * @return int
          */
         public function getRequestId(): int
@@ -27,6 +29,12 @@
         }
 
         /**
+         * Identifier of the shared chat. This number may have more than 32 significant bits and some programming
+         * languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits,
+         * so a 64-bit integer or double-precision float type are safe for storing this identifier.
+         * The bot may not have access to the chat and could be unable to use this identifier, unless the chat is
+         * already known to the bot by some other means.
+         *
          * @return int
          */
         public function getChatId(): int
