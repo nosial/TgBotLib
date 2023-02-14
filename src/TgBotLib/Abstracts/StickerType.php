4fbe285104bb9910648b18b0acb7b2b80f6dfe4e
Netkas <zixing.narrakas1996@gmail.com>
2023-02-14 13:08:16 -0500

    Added \TgBotLib\Abstracts > StickerType

---
+++ src/TgBotLib/Abstracts/StickerType.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace TgBotLib\Abstracts;
+
+    abstract class StickerType
+    {
+        const Regular = 'regular';
+
+        const Mask = 'mask';
+
+        const CustomEmoji = 'custom_emoji';
+    }
