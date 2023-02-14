b401c72b1b5d840ec022e58be26654e776495177
Netkas <zixing.narrakas1996@gmail.com>
2023-02-13 20:29:16 -0500

    Added \TgBotLib\Objects\BotCommandScope > BotCommandScopeAllChatAdministrators

---
+++ src/TgBotLib/Objects/BotCommandScope/BotCommandScopeAllChatAdministrators.php
@@ -0,0 +1,67 @@
+<?php
+
+    /** @noinspection PhpMissingFieldTypeInspection */
+
+    namespace TgBotLib\Objects\BotCommandScope;
+
+    use TgBotLib\Interfaces\ObjectTypeInterface;
+
+    class BotCommandScopeAllChatAdministrators implements ObjectTypeInterface
+    {
+        /**
+         * @var string
+         */
+        private $type;
+
+        /**
+         * @var int|string
+         */
+        private $chat_id;
+
+        /**
+         * Scope type, must be chat_administrators
+         *
+         * @return string
+         */
+        public function getType(): string
+        {
+            return $this->type;
+        }
+
+        /**
+         * Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
+         *
+         * @return int|string
+         */
+        public function getChatId(): int|string
+        {
+            return $this->chat_id;
+        }
+
+        /**
+         * Returns an array representation of the object
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return [
+                'type' => $this->type,
+                'chat_id' => $this->chat_id
+            ];
+        }
+
+        /**
+         * Constructs object from an array representation
+         *
+         * @param array $data
+         * @return ObjectTypeInterface
+         */
+        public static function fromArray(array $data): ObjectTypeInterface
+        {
+            $object = new self();
+            $object->type = $data['type'];
+            $object->chat_id = $data['chat_id'];
+            return $object;
+        }
+    }
