46003c317b864758a3d252867fd00d98db877089
Netkas <zixing.narrakas1996@gmail.com>
2023-02-14 14:33:49 -0500

    Added \TgBotLib\Abstracts > MenuButtonType

---
+++ src/TgBotLib/Abstracts/MenuButtonType.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace TgBotLib\Abstracts;
+
+    abstract class MenuButtonType
+    {
+        const Commands = 'commands';
+        const WebApp = 'web_app';
+        const Default = 'default';
+
+    }
