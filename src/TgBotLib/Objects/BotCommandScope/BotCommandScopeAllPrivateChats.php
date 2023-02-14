e90b920df30f84e9fdf0db7f90461935846f154c
Netkas <zixing.narrakas1996@gmail.com>
2023-02-13 20:22:27 -0500

    Added \TgBotLib\Objects\BotCommandScope > BotCommandScopeAllPrivateChats

---
+++ src/TgBotLib/Objects/BotCommandScope/BotCommandScopeAllPrivateChats.php
@@ -0,0 +1,52 @@
+<?php
+
+    /** @noinspection PhpMissingFieldTypeInspection */
+
+    namespace TgBotLib\Objects\BotCommandScope;
+
+    use TgBotLib\Interfaces\ObjectTypeInterface;
+
+    class BotCommandScopeAllPrivateChats implements ObjectTypeInterface
+    {
+        /**
+         * @var string
+         */
+        private $type;
+
+        /**
+         * Scope type, must be all_private_chats
+         *
+         * @return string
+         */
+        public function getType(): string
+        {
+            return $this->type;
+        }
+
+        /**
+         * Returns an array representation of the object
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return [
+                'type' => $this->type
+            ];
+        }
+
+        /**
+         * Constructs object from an array representation
+         *
+         * @param array $data
+         * @return ObjectTypeInterface
+         */
+        public static function fromArray(array $data): ObjectTypeInterface
+        {
+            $object = new self();
+
+            $object->type = $data['type'];
+
+            return $object;
+        }
+    }
