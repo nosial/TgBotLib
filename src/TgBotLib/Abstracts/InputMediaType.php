c4e12fb39c1c6526cadcd3027903f5cdc2926b15
Netkas <zixing.narrakas1996@gmail.com>
2023-02-14 14:33:58 -0500

    Added \TgBotLib\Abstracts > InputMediaType

---
+++ src/TgBotLib/Abstracts/InputMediaType.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace TgBotLib\Abstracts;
+
+    abstract class InputMediaType
+    {
+        const Photo = 'photo';
+        const Video = 'video';
+        const Animation = 'animation';
+        const Audio = 'audio';
+        const Document = 'document';
+    }
