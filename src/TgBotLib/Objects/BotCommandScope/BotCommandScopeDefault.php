e6ec04b11e6ab78788c3725471b9a250f5f26d5c
Netkas <zixing.narrakas1996@gmail.com>
2023-02-13 20:20:29 -0500

    Added \TgBotLib\Objects\BotCommandScope > BotCommandScopeDefault

---
+++ src/TgBotLib/Objects/BotCommandScope/BotCommandScopeDefault.php
@@ -0,0 +1,68 @@
+<?php
+
+    /** @noinspection PhpMissingFieldTypeInspection */
+
+    namespace TgBotLib\Objects\BotCommandScope;
+
+    use TgBotLib\Interfaces\ObjectTypeInterface;
+    use TgBotLib\Objects\BotCommandScope;
+
+    class BotCommandScopeDefault implements ObjectTypeInterface
+    {
+        /**
+         * @var string
+         */
+        private $type;
+
+        /**
+         * Scope type, must be default
+         *
+         * @return string
+         */
+        public function getType(): string
+        {
+            return $this->type;
+        }
+
+        /**
+         * Returns an array representation of the object
+         *
+         * @return string[]
+         */
+        public function toArray(): array
+        {
+            return [
+                'type' => $this->type
+            ];
+        }
+
+        /**
+         * Constructs object from an array representation
+         *
+         * @param array $data
+         * @return ObjectTypeInterface
+         */
+        public static function fromArray(array $data): ObjectTypeInterface
+        {
+            $object = new self();
+
+            $object->type = $data['type'];
+
+            return $object;
+        }
+
+        /**
+         * Constructs object from BotCommandScope
+         *
+         * @param BotCommandScope $botCommandScope
+         * @return BotCommandScopeDefault
+         */
+        public static function fromBotCommandScope(BotCommandScope $botCommandScope): BotCommandScopeDefault
+        {
+            $object = new self();
+
+            $object->type = $botCommandScope->getType();
+
+            return $object;
+        }
+    }
