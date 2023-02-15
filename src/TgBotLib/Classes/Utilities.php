b2dbc011937b83682c52810f249caa741532cd02
Netkas <zixing.narrakas1996@gmail.com>
2023-02-14 20:57:15 -0500

    More changes

---
+++ src/TgBotLib/Classes/Utilities.php
@@ -0,0 +1,8 @@
+<?php
+
+    namespace TgBotLib\Classes;
+
+    class Utilities
+    {
+
+    }
---
+++ src/TgBotLib/Exceptions/RequestException.php
@@ -0,0 +1,22 @@
+
+<?php
+
+    namespace TgBotLib\Exceptions;
+
+    use Exception;
+    use Throwable;
+
+    class RequestException extends Exception
+    {
+        /**
+         * Public Constructor
+         *
+         * @param string $message
+         * @param int $code
+         * @param Throwable|null $previous
+         */
+        public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+        {
+            parent::__construct($message, $code, $previous);
+        }
+    }
