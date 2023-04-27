581fa42715d309c834de4b62f899d9239ec53e53
Netkas <netkas@nosial.net>
2023-04-27 14:23:14 -0400

    Refactored \TgBotLib\Objects\Telegram\InputMessageContent\InputContactMessageContent and added
     - \TgBotLib\Objects\Telegram\InputMessageContent\InputContactMessageContent::setPhoneNumber
     - \TgBotLib\Objects\Telegram\InputMessageContent\InputContactMessageContent::setFirstName
     - \TgBotLib\Objects\Telegram\InputMessageContent\InputContactMessageContent::setLastName
     - \TgBotLib\Objects\Telegram\InputMessageContent\InputContactMessageContent::setVcard
    
    https://git.n64.cc/nosial/libs/tgbot/-/issues/3

--- src/TgBotLib/Objects/Telegram/InputMessageContent/InputContactMessageContent.php
+++ src/TgBotLib/Objects/Telegram/InputMessageContent/InputContactMessageContent.php
@@ -1,9 +1,12 @@
 <?php
 
+    /** @noinspection PhpUnused */
     /** @noinspection PhpMissingFieldTypeInspection */
 
     namespace TgBotLib\Objects\Telegram\InputMessageContent;
 
+    use InvalidArgumentException;
+    use TgBotLib\Classes\Validate;
     use TgBotLib\Interfaces\ObjectTypeInterface;
 
     class InputContactMessageContent implements ObjectTypeInterface
@@ -39,6 +42,22 @@
         }
 
         /**
+         * Sets the value of 'phone_number' property
+         * Contact's phone number
+         *
+         * @param string $phone_number
+         * @return $this
+         */
+        public function setPhoneNumber(string $phone_number): self
+        {
+            if(!Validate::length($phone_number, 1, 255))
+                throw new InvalidArgumentException('phone_number should be between 1-255 characters');
+
+            $this->phone_number = $phone_number;
+            return $this;
+        }
+
+        /**
          * Contact's first name
          *
          * @return string
@@ -46,6 +65,22 @@
         public function getFirstName(): string
         {
             return $this->first_name;
+        }
+
+        /**
+         * Sets the value of 'first_name' property
+         * Contact's first name
+         *
+         * @param string $first_name
+         * @return $this
+         */
+        public function setFirstName(string $first_name): self
+        {
+            if(!Validate::length($first_name, 1, 255))
+                throw new InvalidArgumentException('first_name should be between 1-255 characters');
+
+            $this->first_name = $first_name;
+            return $this;
         }
 
         /**
@@ -59,6 +94,28 @@
         }
 
         /**
+         * Sets the value of 'last_name' property
+         * Optional. Contact's last name
+         *
+         * @param string|null $last_name
+         * @return InputContactMessageContent
+         */
+        public function setLastName(?string $last_name): self
+        {
+            if($last_name == null)
+            {
+                $this->last_name = null;
+                return $this;
+            }
+
+            if(!Validate::length($last_name, 1, 255))
+                throw new InvalidArgumentException('last_name should be between 1-255 characters (or null)');
+
+            $this->last_name = $last_name;
+            return $this;
+        }
+
+        /**
          * Optional. Additional data about the contact in the form of a vCard, 0-2048 bytes
          *
          * @see https://en.wikipedia.org/wiki/VCard
@@ -67,6 +124,28 @@
         public function getVcard(): ?string
         {
             return $this->vcard;
+        }
+
+        /**
+         * Sets the value of 'vcard' property
+         * Optional. Additional data about the contact in the form of a vCard, 0-2048 bytes
+         *
+         * @param string|null $vcard
+         * @return InputContactMessageContent
+         */
+        public function setVcard(?string $vcard): self
+        {
+            if($vcard == null)
+            {
+                $this->vcard = null;
+                return $this;
+            }
+
+            if(!Validate::length($vcard, 1, 2048))
+                throw new InvalidArgumentException('vcard should be between 1-2048 characters (or null)');
+
+            $this->vcard = $vcard;
+            return $this;
         }
 
         /**
