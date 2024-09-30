fb381c5138c8afe37c70d99807a72b9bf0277a87
netkas <netkas@n64.cc>
2024-09-30 02:35:54 -0400

    Add GetMe method to TgBotLib

---
+++ src/TgBotLib/Methods/GetMe.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TgBotLib\Methods;
+
+use TgBotLib\Abstracts\Method;
+use TgBotLib\Bot;
+use TgBotLib\Enums\Methods;
+use TgBotLib\Objects\Telegram\User;
+
+class GetMe extends Method
+{
+    /**
+     * @inheritDoc
+     */
+    public static function execute(Bot $bot, array $parameters=[]): User
+    {
+        return User::fromArray(self::executeCurl(self::buildPost($bot, Methods::GET_ME->value)));
+    }
+}
