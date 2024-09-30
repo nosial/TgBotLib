9c4e964a15d10408e3bbbdb45cdc85d4fa316cf7
netkas <netkas@n64.cc>
2024-09-30 12:22:02 -0400

    Add Close method

---
+++ src/TgBotLib/Methods/Close.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TgBotLib\Methods;
+
+use TgBotLib\Abstracts\Method;
+use TgBotLib\Bot;
+use TgBotLib\Enums\Methods;
+
+class Close extends Method
+{
+    /**
+     * @inheritDoc
+     */
+    public static function execute(Bot $bot, array $parameters = []): bool
+    {
+        return (bool) self::executeCurl(self::buildPost($bot, Methods::CLOSE->value));
+    }
+}
