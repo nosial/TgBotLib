67230e35bad68f00d908ac71ce543dc7c539fcb7
netkas <netkas@n64.cc>
2024-09-30 12:22:06 -0400

    Add Logout method

---
+++ src/TgBotLib/Methods/Logout.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TgBotLib\Methods;
+
+use TgBotLib\Abstracts\Method;
+use TgBotLib\Bot;
+use TgBotLib\Enums\Methods;
+
+class Logout extends Method
+{
+    public static function execute(Bot $bot, array $parameters=[]): bool
+    {
+        return (bool) self::executeCurl(self::buildPost($bot, Methods::LOGOUT->value));
+    }
+}
