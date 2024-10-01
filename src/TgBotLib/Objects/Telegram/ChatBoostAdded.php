054f76d144b3b5a4ebf91cf48bfc6fac789e9e9d
netkas <netkas@n64.cc>
2024-10-01 12:42:52 -0400

    Added ChatBoostAdded

---
+++ src/TgBotLib/Objects/Telegram/ChatBoostAdded.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace TgBotLib\Objects\Telegram;
+
+use TgBotLib\Interfaces\ObjectTypeInterface;
+
+class ChatBoostAdded implements ObjectTypeInterface
+{
+    private int $boost_count;
+
+    /**
+     * Number of boosts added by the user
+     *
+     * @return int
+     */
+    public function getBoostCount(): int
+    {
+        return $this->boost_count;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        return [
+            'boost_count' => $this->boost_count
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function fromArray(array $data): ObjectTypeInterface
+    {
+        $object = new self();
+        $object->boost_count = $data['boost_count'];
+        return $object;
+    }
+}
