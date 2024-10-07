4d45add53eb0c6517351f5fc6cc76787f4325a40
netkas <netkas@n64.cc>
2024-10-07 19:33:26 -0400

    Updated SendMessage

--- src/TgBotLib/Methods/SendMessage.php
+++ src/TgBotLib/Methods/SendMessage.php
@@ -20,16 +20,6 @@
          */
         public static function execute(Bot $bot, array $parameters=[]): Message
         {
-            if(!isset($parameters['chat_id']))
-            {
-                throw new InvalidArgumentException('chat_id is required');
-            }
-
-            if(!isset($parameters['text']))
-            {
-                throw new InvalidArgumentException('text is required');
-            }
-
             if(isset($parameters['parse_mode']) && $parameters['parse_mode'] instanceof ParseMode)
             {
                 $parameters['parse_mode'] = $parameters['parse_mode']->value;
