95346f78cd0a0558b866fa93faac05f83de5c02c
netkas <netkas@n64.cc>
2024-11-01 18:04:02 -0400

    Add UpdateEvent abstract class

---
+++ src/TgBotLib/Abstracts/UpdateEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+    namespace TgBotLib\Abstracts;
+
+    use TgBotLib\Bot;
+    use TgBotLib\Enums\EventType;
+    use TgBotLib\Objects\Update;
+
+    abstract class UpdateEvent
+    {
+        protected Update $update;
+
+        /**
+         * Constructor for the class.
+         *
+         * @param Update $update The update instance to be used.
+         * @return void
+         */
+        public function __construct(Update $update)
+        {
+            $this->update = $update;
+        }
+
+        /**
+         * Retrieves the event type.
+         * @return EventType The event type of the current instance.
+         */
+        public static function getEventType(): EventType
+        {
+            return EventType::UPDATE_EVENT;
+        }
+
+        public abstract function handle(Bot $bot): void;
+    }
