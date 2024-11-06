307521debc55262770f450d6bd2437dfc1c60ea7
netkas <netkas@n64.cc>
2024-11-06 00:23:10 -0500

    Add SetChatPermissions method to TgBotLib

---
+++ src/TgBotLib/Methods/SetChatPermissions.php
@@ -0,0 +1,50 @@
+<?php
+
+    namespace TgBotLib\Methods;
+
+    use TgBotLib\Abstracts\Method;
+    use TgBotLib\Bot;
+    use TgBotLib\Enums\Methods;
+    use TgBotLib\Interfaces\ObjectTypeInterface;
+
+    class SetChatPermissions extends Method
+    {
+
+        /**
+         * @inheritDoc
+         */
+        public static function execute(Bot $bot, array $parameters = []): true
+        {
+            if($parameters['permissions'] instanceof ObjectTypeInterface)
+            {
+                $parameters['permissions'] = json_encode($parameters['permissions']->toArray());
+            }
+            else
+            {
+                $parameters['permissions'] = json_encode($parameters['permissions']);
+            }
+
+            return (bool)$bot->sendRequest(Methods::SET_CHAT_PERMISSIONS->value, $parameters);
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getRequiredParameters(): ?array
+        {
+            return [
+                'chat_id',
+                'permissions'
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getOptionalParameters(): ?array
+        {
+            return [
+                'use_independent_chat_permissions'
+            ];
+        }
+    }
